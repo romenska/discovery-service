919fc1f22c89466af9c47ae8957938f75901250a
Romenska Olha <romenska.olha@gmail.com>
2022-10-25 23:00:17 +0300

    Create discovery-service

---
+++ src/main/java/romenska/olha/discoveryservice/DiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package romenska.olha.discoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServiceApplication.class, args);
+    }
+
+}
