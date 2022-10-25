2a8381a916f5944bd09974425d6b0a7f242e6a98
Romenska Olha <romenska.olha@gmail.com>
2022-10-25 23:01:52 +0300

    Resolving eureka endpoints for persons-service

--- src/main/java/romenska/olha/discoveryservice/DiscoveryServiceApplication.java
+++ src/main/java/romenska/olha/discoveryservice/DiscoveryServiceApplication.java
@@ -2,7 +2,9 @@ package romenska.olha.discoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class DiscoveryServiceApplication {
 
